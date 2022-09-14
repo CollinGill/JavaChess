b3610de71015f58114a6e8a857442186dd3bf773
CollinGill <collingill02@gmail.com>
2022-09-14 15:22:34 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# JavaChess
+
+JavaChess is a UCI compliant chess GUI made using JavaFX and built with Maven.
+
+## Building
+
+```shell
+$ mvn javafx:run
+```
---
+++ src/main/java/com/example/javachess/JavaChess.java
@@ -0,0 +1,23 @@
+package com.example.javachess;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class JavaChess extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(JavaChess.class.getResource("javaChessGUI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("JavaChess");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
