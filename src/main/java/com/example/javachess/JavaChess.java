2ade2614bd12006b8b59d60a742ca4df657c0bd4
CollinGill <collingill02@gmail.com>
2022-09-14 15:36:10 -0400

    fix sizing issue

--- src/main/java/com/example/javachess/JavaChess.java
+++ src/main/java/com/example/javachess/JavaChess.java
@@ -11,7 +11,7 @@ public class JavaChess extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(JavaChess.class.getResource("javaChessGUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 640, 400);
         stage.setTitle("JavaChess");
         stage.setScene(scene);
         stage.show();
